48655727540f27e67da030ca4e934ad715158ec8
Aline de Morais <morais.alinede@gmail.com>
2025-04-26 14:42:53 -0300

    teste automatizado exercício módulo 12

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 let dadosLogin
+import { faker } from '@faker-js/faker';
 
 context('Funcionalidade Login', () => {
     before(() => {
@@ -17,22 +18,30 @@ context('Funcionalidade Login', () => {
     });
 
     it('Login com sucesso usando Comando customizado', () => {
-        cy.login(dadosLogin.usuario, dadosLogin.senha)
+        cy.login('aluno_ebac@teste.com', 'teste@teste.com')
         cy.get('.page-title').should('contain', 'Minha conta')
+        cy.cadastro(faker.person.firstName(), faker.person.lastName())
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta')
     });
 
     it('Login usando fixture', () => {
-        cy.fixture('perfil').then((dados) => {
-            cy.login(dados.usuario, dados.senha)
+        cy.fixture('perfil').then(login => {
+            cy.login(login.usuario, login.senha)
         })
         cy.get('.page-title').should('contain', 'Minha conta')
     });
 
-    it.skip('Deve fazer login com sucesso - sem otimização', () => {
-        cy.get('#username').type(dadosLogin.usuario)
-        cy.get('#password').type(dadosLogin.senha, { log: false })
+    it('Deve fazer login com sucesso - sem otimização', () => {
+       cy.get('#username').type('aluno_ebac@teste.com')
+       cy.get('#password').type('teste@teste.com', { log: false })
         cy.get('.woocommerce-form > .button').click()
         cy.get('.page-title').should('contain', 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
-    })
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').clear().type('Aline')
+        cy.get('#account_last_name').clear().type('de Morais')
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('exist')
+    });
+
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,3 +4,9 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('cadastro', (nome, sobrenome) => {
+    cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+    cy.get('#account_first_name').clear().type(nome)
+    cy.get('#account_last_name').clear().type(sobrenome)
+    cy.get('.woocommerce-Button').click()
+});
